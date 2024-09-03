52fef956f971b87e53a620e852638b55d20ed84e
anjo2929 <anjotelo75@gmail.com>
2024-09-03 10:42:42 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] classmates = { "Clyde", "Oswell", "Reynold", "Larry", "Voltier", "Anjo" };
+
+           
+            foreach (string name in classmates)
+            {
+                Console.WriteLine(name);
+            }
+
+            char[,] a = new char[2, 3]
+            {
+                {'a','b','c'},
+                {'d','e','f'}
+            };
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+                string message = "hello, ang inet";
+                Console.WriteLine(message.Contains("hello"));
+
+
+            
+        }
+    }
+}
